77a4aac884428a4b90210db7a8fecfb602cc21cf
Rachel Fritz <rachelfritz0@gmail.com>
2020-09-30 06:00:11 -0500

    added ReactionSchema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,35 @@
 const { Schema, model, Types } = require('mongoose');
+// import moment for the date to read correctly
 const moment = require('moment');
+
+const ReactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+            trim: true
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            defualt: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
 
 const ThoughtSchema = new Schema(
     {
@@ -30,3 +60,9 @@ const ThoughtSchema = new Schema(
         id: false
     }
 );
+
+// create the Thought model using the ThoughtSchema
+const Thought = model('ThoughtSchema')
+
+// export the Thought model
+module.exports = Thought;
