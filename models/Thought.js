da933ccd4168c6e006d262a9bf9cafbf7253ac5f
Rachel Fritz <rachelfritz0@gmail.com>
2020-09-29 20:38:53 -0500

    setting up models

---
+++ models/Thought.js
@@ -0,0 +1,22 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+    }
+)
